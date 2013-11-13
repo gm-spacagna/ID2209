4c9f839b98f76324d0b89d29f19125e1da41ceab
Gianmario Spacagna <gspacagn@cisco.com>
2013-11-13 15:09:23 +0000

    Added Gianmario Agent.

---
+++ src/agents/GianmarioAgent.java
@@ -0,0 +1,47 @@
+package agents;
+
+import jade.core.Agent;
+import jade.core.behaviours.SimpleBehaviour;
+
+/**
+ * Gianmario's Agent. Simple example class.
+ * 
+ * @author Gianmario Spacagna (gspacagn@cisco.com)
+ */
+public class GianmarioAgent extends Agent {
+
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 9101089205025155960L;
+
+  @Override
+  protected void setup() {
+    super.setup();
+    addBehaviour(new GianmarioBehaviour(this));
+  }
+
+  class GianmarioBehaviour extends SimpleBehaviour {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -205710971451777349L;
+
+    public GianmarioBehaviour(GianmarioAgent gianmarioAgent) {
+      super(gianmarioAgent);
+    }
+
+    private boolean finished;
+    
+    @Override
+    public void action() {
+      finished = false;
+    }
+
+    @Override
+    public boolean done() {
+      return finished;
+    }
+  }
+}
